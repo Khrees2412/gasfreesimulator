6ca151b31803a26290737560dc0d7aa3730f2f93
Christian Ndu <christiannduh@gmail.com>
2025-02-23 08:32:23 +0100

    smart contract done

---
+++ README.md
@@ -0,0 +1,1 @@
+Gasless Wallet Simulator
---
+++ src/lib.rs
@@ -0,0 +1,86 @@
+#![cfg_attr(not(any(test, feature = "export-abi")), no_main)]
+extern crate alloc;
+
+use alloy_primitives::Address;
+use alloy_sol_types::sol;
+use stylus_sdk::stylus_core::log;
+use stylus_sdk::{alloy_primitives::U256, prelude::*};
+
+sol_storage! {
+    #[entrypoint]
+    pub struct GasFreeSimulator {
+        uint256 number;
+    }
+}
+
+sol! {
+    event Log(address indexed sender, string message);
+}
+
+#[public]
+impl GasFreeSimulator {
+    pub fn simulate_tx(&self, caller: Address, tx_type: u8) {
+        let gas_before = self.vm().evm_gas_left();
+
+        match tx_type {
+            1 => self.dummy_transfer(),
+            2 => self.dummy_swap(),
+            3 => self.dummy_contract_interaction(),
+            _ => log(
+                self.vm(),
+                Log {
+                    sender: caller,
+                    message: "Invalid transaction type".to_string(),
+                },
+            ),
+        }
+
+        let gas_after = self.vm().evm_gas_left();
+        let gas_used = gas_before - gas_after;
+
+        log(
+            self.vm(),
+            Log {
+                sender: caller,
+                message: format!("Tx Type: {}, Gas used: {} by {}", tx_type, gas_used, caller),
+            },
+        );
+    }
+
+    fn dummy_transfer(&self) {
+        let _ = self.vm().evm_gas_left() / 2;
+    }
+
+    fn dummy_swap(&self) {
+        let _ = self.vm().evm_gas_left() / 3;
+    }
+
+    fn dummy_contract_interaction(&self) {
+        let _ = self.vm().evm_gas_left() / 4;
+    }
+
+    pub fn estimate_paymaster_cost(&self, tx_type: u8, num_txs: u32) {
+        let base_gas_cost = match tx_type {
+            1 => 21000, // Approx for transfers
+            2 => 45000, // Approx for swaps
+            3 => 60000, // Approx for contract calls
+            _ => 0,
+        };
+
+        let total_gas = U256::from(base_gas_cost) * U256::from(num_txs);
+
+        let gas_price = U256::from(1_000_000_000_u64); // Example: 1 Gwei
+        let wei_required = total_gas * gas_price; // No floating-point needed
+
+        log(
+            self.vm(),
+            Log {
+                sender: Address::default(),
+                message: format!(
+                    "Estimated cost for {} transactions of type {}: {} wei",
+                    num_txs, tx_type, wei_required
+                ),
+            },
+        );
+    }
+}
